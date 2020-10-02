b515a99432cb276f3136417d670d0e3d32e4480a
kienboeck <kienboec@technikum-wien.at>
2020-10-02 13:05:53 +0200

    work in progress Highscore list with MVVM pattern (no Command Handling yet)

---
+++ HighScores/SWA.Highscores.InClass/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SWA.Highscores.InClass
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _playerName = "no player";
+
+        public string PlayerName
+        {
+            get
+            {
+                return _playerName;
+            }
+            set
+            {
+                _playerName = value;
+                if (_playerName == "Daniel")
+                {
+                    Points = 9001;
+                }
+                OnPropertyChanged("PlayerName"); // use nameof
+            }
+        }
+
+        private int _points;
+
+        public int Points
+        {
+            get
+            {
+                return _points;
+            }
+            set
+            {
+                _points = value;
+                OnPropertyChanged("Points");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ HighScores/SWA.Highscores.InClass/Model/HighScoreItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWA.Highscores.InClass.Model
+{
+    public class HighScoreItem
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+    }
+}
