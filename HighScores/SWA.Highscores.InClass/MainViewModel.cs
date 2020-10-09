a32dc0d65c5903ff8ad104a7d4cf0865170e8c44
kienboeck <kienboec@technikum-wien.at>
2020-10-09 12:52:42 +0200

    InClass Version 2020-10-09

--- HighScores/SWA.Highscores.InClass/MainViewModel.cs
+++ HighScores/SWA.Highscores.InClass/MainViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using SWA.Highscores.InClass.Model;
 
 namespace SWA.Highscores.InClass
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<HighScoreItem> HighScoreItems { get; set; }
+
+        #region Properties for Textboxes
+
         private string _playerName = "no player";
 
         public string PlayerName
@@ -40,6 +46,37 @@ namespace SWA.Highscores.InClass
                 _points = value;
                 OnPropertyChanged("Points");
             }
+        }
+
+        #endregion
+
+        public RelayCommand AddCommand { get; set; }
+
+        public MainViewModel()
+        {
+            HighScoreItems = new ObservableCollection<HighScoreItem>();
+            HighScoreItems.Add(new HighScoreItem()
+            {
+                Name = "Isabella", Points = 800
+            });
+            HighScoreItems.Add(new HighScoreItem()
+            {
+                Name = "Benedikt R", Points = 1340
+            });
+
+            // AddCommand = new RelayCommand(new Action<object>(Add)); // long version
+            // AddCommand = new RelayCommand((parameter) => Add(parameter));
+            AddCommand = new RelayCommand(Add, (parameter) => this.Points > 0); // short version
+        }
+
+        private void Add(object parameter)
+        {
+            // Observable Collection does the magic to update UI
+            HighScoreItems.Add(new HighScoreItem()
+            {
+                Name = this.PlayerName,
+                Points = this.Points,
+            });
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ HighScores/SWA.Highscores.InClass/OverOneThousandIntToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SWA.Highscores.InClass
+{
+    public class OverOneThousandIntToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                int intValue = (int)value;
+                if (intValue < 1000)
+                {
+                    return Brushes.Red;
+                } 
+                
+                return Brushes.LightGreen;
+            }
+
+            return Brushes.GhostWhite;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // not used... exception is fine here
+            throw new NotImplementedException();
+        }
+    }
+}
