d582f19fb4de121652b7b8ea52ce93984fb5425f
kienboeck <kienboec@technikum-wien.at>
2020-10-15 17:55:30 +0200

    MVVM-Light sample that corresponds to the video (code also in moodle)

---
+++ MVVMLightSampleDotNetCore2/MVVMLightSampleDotNetCore2/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MVVMLightSampleDotNetCore2
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _title;
+
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Title = "Main (Design)";
+            }
+            else
+            {
+                Title = "Main (Live)";
+            }
+
+            SetTitleCommand = new RelayCommand(
+                () =>
+                {
+                    Title = $"Changed at {DateTime.Now.Second}";
+                });
+        }
+
+        public RelayCommand SetTitleCommand { get; set; }= null;
+        public string Title
+        {
+            get => _title; 
+            set => Set(ref _title, value, nameof(Title)); 
+        }
+    }
+}
