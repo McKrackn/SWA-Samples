df93bcce549a0b1528cc69cafe23901fb4d39df6
kienboeck <kienboec@technikum-wien.at>
2020-11-13 12:44:54 +0100

    in-class development styles/triggers

---
+++ WPFButtonGallery/WPFButtonGallery/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace WPFButtonGallery
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private bool _isSpecial = true;
+
+        public bool IsSpecial
+        {
+            get => _isSpecial;
+            set => this.Set(ref _isSpecial, value, nameof(IsSpecial));
+        }
+    }
+}
---
+++ WPFButtonGallery/WPFButtonGallery/ViewModelLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace WPFButtonGallery
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+        public MainViewModel Main => SimpleIoc.Default.GetInstance<MainViewModel>();
+    }
+}
