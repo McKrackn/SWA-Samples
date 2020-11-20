1a9a93731a26f77181766913d08122d928f863c6
kienboeck <kienboec@technikum-wien.at>
2020-11-20 11:38:43 +0100

    DesignVM, InteractionTriggers added to WPF-Button Gallery

--- WPFButtonGallery/WPFButtonGallery/MainViewModel.cs
+++ WPFButtonGallery/WPFButtonGallery/MainViewModel.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace WPFButtonGallery
 {
     public class MainViewModel : ViewModelBase
     {
         private bool _isSpecial = true;
+        private string _state= "created...";
 
         public bool IsSpecial
         {
             get => _isSpecial;
             set => this.Set(ref _isSpecial, value, nameof(IsSpecial));
+        }
+
+        public string State
+        {
+            get { return _state; }
+            set { this.Set(ref _state, value, nameof(State)); }
+        }
+
+        public RelayCommand SquidwardTentaclesActivatedCommand { get; }
+
+        public MainViewModel()
+        {
+            SquidwardTentaclesActivatedCommand = new RelayCommand(() => State = "Squidward got focus");
         }
     }
 }
---
+++ WPFButtonGallery/WPFButtonGallery/MainViewModelDesign.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFButtonGallery
+{
+    public class MainViewModelDesign : MainViewModel
+    {
+        public MainViewModelDesign()
+        {
+            this.IsSpecial = false;
+            this.State = "very long text";
+        }
+    }
+}
