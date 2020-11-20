5adcf32f106898680b006942411fa654cc210f1b
kienboeck <kienboec@technikum-wien.at>
2020-11-20 14:12:42 +0100

    second project with itemsControl and DataTemplates

---
+++ WPFButtonGallery/WPFButtonGallery2/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace WPFButtonGallery2
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<SBCharacter> Characters { get; } = new ObservableCollection<SBCharacter>();
+        public MainViewModel()
+        {
+            Characters.Add(new SBCharacter(){ Id = 1, Name = "SpongeBob", Job = "Cook"});
+            Characters.Add(new SBCharacter(){ Id = 2, Name = "Patrick", Job = "unemployed"});
+            Characters.Add(new SBCharacter(){ Id = 3, Name = "Squidward", Job = "Cashier" });
+        }
+    }
+}
---
+++ WPFButtonGallery/WPFButtonGallery2/SBCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace WPFButtonGallery2
+{
+    public class SBCharacter : ViewModelBase
+    {
+        private int _id;
+        private string _name;
+        private string _job;
+
+        public int Id
+        {
+            get { return _id; }
+            set { this.Set(ref _id, value, nameof(Id)); }
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set => this.Set(ref _name, value, nameof(Name));
+        }
+
+        public string Job
+        {
+            get { return _job; }
+            set 
+            {
+                this.Set(ref _job, value, nameof(Job));
+            }
+        }
+
+    }
+}
